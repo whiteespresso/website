2b4209b60612ab80b776fa7a58bd5c78cbade278
Ada <adazych@wp.pl>
2015-05-21 18:55:39 +0200

    first commit

---
+++ js/myJS.js
@@ -0,0 +1,25 @@
+
+// $(document).ready();
+// var causeRepaintsOn = $('.score-text');
+
+// $(window).resize(function () {
+//     causeRepaintsOn.css("z-index;
+// });
+
+    $(document).ready(function(){
+      $('a[href*=#]:not([href=#myCarousel])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+          var $target = $(this.hash);
+          $target = $target.length && $target
+          || $('[name=' + this.hash.slice(1) +']');
+          if ($target.length) {
+            var targetOffset = $target.offset().top;
+            $('html,body')
+            .animate({scrollTop: targetOffset}, 1500);
+           return false;
+          }
+        }
+      });
+    });
+
